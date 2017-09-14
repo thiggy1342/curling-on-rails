c4d362820e9b5659c0140e5680f10c3af6da3d28
thiggy1342 <d.adam.thigpen@gmail.com>
2017-09-14 13:36:07 -0400

    fleshed out basic actions and view for teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -16,12 +16,30 @@ class TeamsController < ApplicationController
   def create
     @league = League.find(params[:league_id])
     @team = @league.teams.create(team_params)
-    redirect_to league_team_path(@league, @team)
+    redirect_to league_teams_path
+  end
+
+  def edit
+    @league = League.find(params[:league_id])
+    @team = @league.teams.find(params[:id])
+  end
+
+  def update
+    @league = League.find(params[:league_id])
+    @team = @league.teams.update(team_params)
+    redirect_to league_team_path(@team)
+  end
+
+  def destroy
+    @league = League.find(params[:league_id])
+    @league.teams.find(params[:id]).destroy
+    redirect_to league_teams_path
   end
 
   private
 
   def team_params
-    params.require(:team).permit(:team_name, :lead, :second, :vice, :skip)
+    params.require(:team).permit(:league_id, :team_name, :lead, :second, :vice, :skip)
   end
+
 end
