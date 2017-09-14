c85efec8f35384fc1dc849a5f61584839a7b9f70
thiggy1342 <d.adam.thigpen@gmail.com>
2017-09-14 09:59:15 -0400

    restructuring routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :leagues do
     resources :games
+    resources :teams do
+      resources :players
+    end
   end
-  resources :teams
-  resources :players
 end
