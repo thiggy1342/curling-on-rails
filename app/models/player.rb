6f07c6a8bdd5701f12bc5a1fb67229224530414a
thiggy1342 <d.adam.thigpen@gmail.com>
2017-09-14 09:12:17 -0400

    removing db associations for now

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,4 @@
 class Player < ApplicationRecord
-	has_and_belongs_to_many :leagues
-	has_and_belongs_to_many :teams
-
 	validates :first_name, presence: true
 	validates :last_name, presence: true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,4 @@
 class Team < ApplicationRecord
-	belongs_to :leagues
-	has_and_belongs_to_many :games
-	has_and_belongs_to_many :players
-
 	validates :lead, presence: true
 	validates :second, presence: true
 	validates :vice, presence: true
