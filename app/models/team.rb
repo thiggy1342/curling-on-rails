9f8baad1217cf3b46af8732a6355d0a0d828125a
thiggy1342 <d.adam.thigpen@gmail.com>
2017-09-14 13:09:38 -0400

    fixed borked has one relationship

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,3 @@
 class Team < ApplicationRecord
-	belongs_to :team
-
-	# validates :lead, presence: true
-	# validates :second, presence: true
-	# validates :vice, presence: true
-	# validates :skip, presence: true
+	belongs_to :league
 end
